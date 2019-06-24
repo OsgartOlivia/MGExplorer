e80947cf80414b924516ecafd6cc559d7de59aa7
Olivia Osgart <olivia.osgart@etu.univ-cotedazur.fr>
2019-06-24 16:52:10 +0200

    Commentaires passés en anglais, début d'ajout des quatres types de documents dans le chart Iris (en recherche encore)

--- iris/js/irisPanel.js
+++ iris/js/irisPanel.js
@@ -1,94 +1,94 @@
 /**
-* irisPanel
-*
-*/
+ * irisPanel
+ *
+ */
 
 define([], function () {
 
-  return function IrisPanel (irisVisChart) {
-	var _irisChart = irisVisChart,
-	    _width = 220,
-		_height = 80,
-		_selectOrder = null,
-		_selectAttr  = null,
-		_vAttrEdgesSelecionaveis = [],
-		_idPanel;     // Atribuido na função create
- 
-    function _addItensSelectOrder() {
-	var selOption;
-	  
-		selOption = d3.select("#" + _idPanel + " .IC-selOrderBy").selectAll("option");
-		if (!selOption.empty())
-			selOption.remove();
+    return function IrisPanel (irisVisChart) {
+        let _irisChart = irisVisChart,
+            _width = 220,
+            _height = 80,
+            _selectOrder = null,
+            _selectAttr  = null,
+            _vAttrEdgesSelecionaveis = [],
+            _idPanel;     // Assigned in create
 
-		_selectOrder.append(new Option("Text", 0));
-		_selectOrder.append(new Option("Attribute", 1));  // 100 começa índice numéricos	  
+        function _addItensSelectOrder() {
+            let selOption;
 
-		_selectOrder[0].selectedIndex = 0;
-	  
-		_selectOrder.change( function(){
-			var valor = +this.value;
-			
-			if (valor==0)
-				_irisChart.acSortExecText();			
-			else
-				_irisChart.acSortExecAttribute();
-		});
+            selOption = d3.select("#" + _idPanel + " .IC-selOrderBy").selectAll("option");
+            if (!selOption.empty())
+                selOption.remove();
 
-	}
-	
-    function _addSelectOrder( idDivPanel) { 
-	  _selectOrder = $("<select>", { class: "IC-selOrderBy"});;
-	  $(idDivPanel).append( $("<br/>") ).append( $("<label>").append("&nbsp;Order by:")).append(_selectOrder);
-	  _addItensSelectOrder();
-	}
-	
-  //-----------------------------------	  
-	
-    function panel() {}
+            _selectOrder.append(new Option("Text", 0));
+            _selectOrder.append(new Option("Attribute", 1));  // 100 starts numeric index
 
-	panel.create = function( idPanel) {
-	  _idPanel = idPanel;
-	  var divPanel = $("<div/>",{
-		  class:"IC-panel"
-	  }).css({"width":_width, "height": _height});
-	  $("#"+_idPanel).append(divPanel);
-	  
-					//------------- Select para a ordem de classificação
-	  _addSelectOrder("#"+ _idPanel + " .IC-panel");
+            _selectOrder[0].selectedIndex = 0;
 
-	  return panel;
-	}
+            _selectOrder.change( function(){
+                let valor = +this.value;
 
-	//---------------------
-	panel.update = function() {
-//	  _addItensSelectOrder();	
-	}
-	
-	//---------------------
-	// Inclui os select para todos os atributos	
+                if (valor===0)
+                    _irisChart.acSortExecText();
+                else
+                    _irisChart.acSortExecAttribute();
+            });
 
-	panel.incluiSelectAttr = function ( vAttrEdgesSelecionaveis) {
-	var i;
-		for (i=0; i<vAttrEdgesSelecionaveis.length; i++)
-			_vAttrEdgesSelecionaveis[i] = vAttrEdgesSelecionaveis[i];
-		_selectAttr = $("<select>", { class: "IC-selAtributo"});
-		$("#"+ _idPanel + " .IC-panel").append( $("<br/>") ).append( $("<label>").append("&nbsp;Show attribute:")).append(_selectAttr);
+        }
 
-		for (i=0; i<vAttrEdgesSelecionaveis.length; i++) {
-			if ( vAttrEdgesSelecionaveis[i] >= 1000)
-				_selectAttr.append(new Option(_irisChart.data().edges.valueTitle[vAttrEdgesSelecionaveis[i]-1000], vAttrEdgesSelecionaveis[i]-1000));
-		}			
+        function _addSelectOrder( idDivPanel) {
+            _selectOrder = $("<select>", { class: "IC-selOrderBy"});
+            $(idDivPanel).append( $("<br/>") ).append( $("<label>").append("&nbsp;Order by:")).append(_selectOrder);
+            _addItensSelectOrder();
+        }
 
-		_selectAttr.change( function(){
-			var valor = +this.value;
-			
-			_irisChart.acChangeAttrBar(valor);			
-		});	
-    }	
-		
-	//---------------------	
-  	return panel; 
-  };
+        //-----------------------------------
 
-})
+        function panel() {}
+
+        panel.create = function( idPanel) {
+            _idPanel = idPanel;
+            let divPanel = $("<div/>",{
+                class:"IC-panel"
+            }).css({"width":_width, "height": _height});
+            $("#"+_idPanel).append(divPanel);
+
+            //------------- Select for sort order
+            _addSelectOrder("#"+ _idPanel + " .IC-panel");
+
+            return panel;
+        };
+
+        //---------------------
+        panel.update = function() {
+            //_addItensSelectOrder();
+        };
+
+        //---------------------
+        // Includes select for all attributes
+
+        panel.incluiSelectAttr = function ( vAttrEdgesSelecionaveis) {
+            let i;
+            for (i=0; i<vAttrEdgesSelecionaveis.length; i++)
+                _vAttrEdgesSelecionaveis[i] = vAttrEdgesSelecionaveis[i];
+            _selectAttr = $("<select>", { class: "IC-selAtributo"});
+            $("#"+ _idPanel + " .IC-panel").append( $("<br/>") ).append( $("<label>").append("&nbsp;Show attribute:")).append(_selectAttr);
+
+            for (i=0; i<vAttrEdgesSelecionaveis.length; i++) {
+                if ( vAttrEdgesSelecionaveis[i] >= 1000)
+                    _selectAttr.append(new Option(_irisChart.data().edges.valueTitle[vAttrEdgesSelecionaveis[i]-1000], vAttrEdgesSelecionaveis[i]-1000));
+            }
+
+            _selectAttr.change( function(){
+                let valor = +this.value;
+
+                _irisChart.acChangeAttrBar(valor);
+            });
+        };
+
+        //---------------------
+        return panel;
+    };
+
+});
